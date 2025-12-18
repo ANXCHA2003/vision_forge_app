b7ddf5cd644b1e2fd2ce1e9da73ac3c068cf3c97
ANXCHA2003 <anucha005500@gmail.com>
2025-12-18 10:04:30 +0700

    นับเลยเวลากด + จะเพิ่มทีล่ะ 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const CounterApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int count = 0;
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: const Text('นับเลขด้วย Flutter 💙')),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'คุณกดปุ่มแล้ว $count ครั้ง',
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: increment,
+          child: const Icon(Icons.add),
         ),
       ),
     );
